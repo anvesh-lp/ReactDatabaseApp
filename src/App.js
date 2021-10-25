f10f7fd89a8635a165ba9e00038f49badb8fe483
anvesh-lp <knanvesh.01@gmail.com>
2021-10-24 23:43:34 -0400

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+
+  const [movies,setMovies]=useState([])
+  
+  async function triggeringFucniton(){
+    const response=await fetch("http://swapi.dev/api/films/");
+    const  data= await response.json();
+
+    const tranforemddata=data.results.map((moviesData)=>{
+      return {
+        id:moviesData.episode_id,
+        title:moviesData.title,
+        openingText:moviesData.opening_crawl,
+        releaseDate:moviesData.release_date,
+      };
+    });
+    setMovies(tranforemddata)
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={triggeringFucniton}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
