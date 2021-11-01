380d608742b65641f8c62ba60ab09d7c89ee031d
anvesh-lp <knanvesh.01@gmail.com>
2021-10-31 21:25:39 -0400

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+import AddMovie from "./components/AddMovie";
 
 function App() {
 
-  const [movies,setMovies]=useState([])
-  
-  async function triggeringFucniton(){
-    const response=await fetch("http://swapi.dev/api/films/");
-    const  data= await response.json();
+    const [movies, setMovies] = useState([])
+    const [error, setError] = useState(null)
+    
+    function addMovieHandler(movie){
+        console.log(movie);
+    }
 
-    const tranforemddata=data.results.map((moviesData)=>{
-      return {
-        id:moviesData.episode_id,
-        title:moviesData.title,
-        openingText:moviesData.opening_crawl,
-        releaseDate:moviesData.release_date,
-      };
-    });
-    setMovies(tranforemddata)
-  }
+    async function triggeringFucniton() {
 
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={triggeringFucniton}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-    </React.Fragment>
-  );
+        try {
+            const response = await fetch("https://react-database-16425-default-rtdb.firebaseio.com/movies.json");
+            if (!response.ok){
+                throw new Error("Invalid error")
+            }
+            const data = await response.json();
+
+            const tranforemddata = data.results.map((moviesData) => {
+                return {
+                    id: moviesData.episode_id,
+                    title: moviesData.title,
+                    openingText: moviesData.opening_crawl,
+                    releaseDate: moviesData.release_date,
+                };
+            });
+            setMovies(tranforemddata)
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <AddMovie onAddMovie={addMovieHandler}/>
+            </section>
+            <section>
+                <button onClick={triggeringFucniton}>Fetch Movies</button>
+            </section>
+            <section>
+                {!error && <MoviesList movies={movies}/>}
+                {error && <p>Cant load movies,invalid error</p>}
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
